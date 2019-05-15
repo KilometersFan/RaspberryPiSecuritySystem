fdcffc45949d1eeef41b918347a3faa1846d81fc
Miles Phan <milespha@usc.edu>
2019-05-15 15:46:16 -0700

    setup sensors and testing code

---
+++ security.py
@@ -0,0 +1,23 @@
+sys.path.append('/home/pi/Dexter/GrovePi/Software/Python')
+import grovepi
+import grove_rgb_lcd as lcd
+
+PORT_BUTTON = 3
+PORT_BUZZER = 4
+PORT_ROTARY = 2
+PORT_RANGE = 7
+PORT_RED_LED = 5
+PORT_GREEN_LED = 6 
+
+grovepi.pinMode(PORT_BUZZER, "OUTPUT")
+grovepi.pinMode(PORT_BUTTON, "INPUT")
+grovepi.pinMode(PORT_ROTARY,"INPUT") 
+grovepi.pinMode(PORT_RED_LED, "OUTPUT")
+grovepi.pinMode(PORT_GREEN_LED, "OUTPUT")
+
+while True:
+	try :
+		grovepi.digitalWrite(PORT_GREEN_LED,1)
+	except KeyboardInterrupt:
+		grovepi.digitalWrite(PORT_GREEN_LED,0)
+		break
